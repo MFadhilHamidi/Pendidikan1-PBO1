87376e7d339d0b6195682b1943c79b4e480f5950
MFadhilHamidi <154021183+MFadhilHamidi@users.noreply.github.com>
2024-06-07 20:14:24 +0530

    Pendidikan1 - PBO1
    Update Method

--- src/MainFile/MainFile.java
+++ src/MainFile/MainFile.java
@@ -16,15 +16,35 @@ public class MainFile {
      */
     public static void main(String[] args) {
         Database dbConnect = new Database();
+        
+        //tb_ulangan
         //dbConnect.hapusTbUlangan("002");
         //dbConnect.ubahTbUlangan("002", "B.ARAB");
+        //dbConnect.tambahTbUlangan("001", "SBD");
+        //dbConnect.tambahTbUlangan("002", "B.INGGRIS");
+        //dbConnect.tambahTbUlangan("003", "SEJARAH");
+        //dbConnect.tambahTbUlangan("004", "IPA");
         //dbConnect.tambahTbUlangan("005", "IPS");
+        
+        //tb_pengampu
         //dbConnect.hapusTbPengampu("030");
         //dbConnect.ubahTbPengampu("030", "235", "0002");
+        //dbConnect.tambahTbPengampu("020", "234", "0001")
+        //dbConnect.tambahTbPengampu("030", "235", "0002");
+        //dbConnect.tambahTbPengampu("040", "236", "0003");
+        //dbConnect.tambahTbPengampu("050", "237", "0004");
         //dbConnect.tambahTbPengampu("060", "238", "0005");
+        
+        //tb_mapel
         //dbConnect.hapusTbMapel("0003");
         //dbConnect.ubahTbMapel("0003", "SOSBUD", "60");
+        //dbConnect.tambahTbMapel("0001", "MTK", "80");
+        //dbConnect.tambahTbMapel("0002", "IPS", "60");
+        //dbConnect.tambahTbMapel("0003", "SEJARAH, "70");
+        //dbConnect.tambahTbMapel("0004", "IPA", "70");
         //dbConnect.tambahTbMapel("0005", "B.INGGRIS", "80");
+        
+        //Data dan Cari
         //dbConnect.dataTbUlangan();
         //dbConnect.cariTbUlangan("001");
         //dbConnect.dataTbPengampu();
@@ -35,8 +55,13 @@ public class MainFile {
         //dbConnect.cariTbGuru("234");
         // TODO code application logic here
         // TODO code application logic here
-        // dbConnect.ubahTbGuru("123", "Fadhl", "Jl.Ngaji", "087645678321", "MTK");
-        // dbConnect.hapusTbGuru("123");
+        
+        //tb_guru
+        //dbConnect.ubahTbGuru("123", "Fadhl", "Jl.Ngaji", "087645678321", "MTK");
+        //dbConnect.hapusTbGuru("123");
+        //dbConnect.tambahTbGuru("234", "Fadhil", "Jl.HKSN", "085823456789", "FTI");
+        //dbConnect.tambahTbGuru("235", "Riski", "Jl.Belwauran", "082123456754", "IPA");
+        //dbConnect.tambahTbGuru("236", "Ahmad", "Jl.Kuala", "085812345678", "FSI");
         //dbConnect.tambahTbGuru("237", "Mahdi", "Jl.Lumpur", "085234567654", "FTI");
     }
     
