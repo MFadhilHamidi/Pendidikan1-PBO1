193c944cfed22db21bc203afb9ccbac6591552eb
MFadhilHamidi <154021183+MFadhilHamidi@users.noreply.github.com>
2024-06-07 19:45:02 +0530

    Update MainFile.java

--- src/MainFile/MainFile.java
+++ src/MainFile/MainFile.java
@@ -18,13 +18,13 @@ public class MainFile {
         Database dbConnect = new Database();
         //dbConnect.hapusTbUlangan("002");
         //dbConnect.ubahTbUlangan("002", "B.ARAB");
-        //dbConnect.tambahTbUlangan("002", "SEJARAH");
+        //dbConnect.tambahTbUlangan("005", "IPS");
         //dbConnect.hapusTbPengampu("030");
         //dbConnect.ubahTbPengampu("030", "235", "0002");
-        //dbConnect.tambahTbPengampu("030", "235", "0002");
+        //dbConnect.tambahTbPengampu("060", "238", "0005");
         //dbConnect.hapusTbMapel("0003");
         //dbConnect.ubahTbMapel("0003", "SOSBUD", "60");
-        //dbConnect.tambahTbMapel("0003", "IPA", "70");
+        //dbConnect.tambahTbMapel("0005", "B.INGGRIS", "80");
         //dbConnect.dataTbUlangan();
         //dbConnect.cariTbUlangan("001");
         //dbConnect.dataTbPengampu();
@@ -37,7 +37,7 @@ public class MainFile {
         // TODO code application logic here
         // dbConnect.ubahTbGuru("123", "Fadhl", "Jl.Ngaji", "087645678321", "MTK");
         // dbConnect.hapusTbGuru("123");
-        // dbConnect.tambahTbGuru("234", "Fadhil", "Jl.HKSN", "085823456789", "FTI");
+        //dbConnect.tambahTbGuru("237", "Mahdi", "Jl.Lumpur", "085234567654", "FTI");
     }
     
 }
